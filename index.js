1bc77da73d99c76e897a1af235f71c5d24c17c41
JoshuaJackson23 <16204844-JoshuaJackson23@users.noreply.replit.com>
2023-05-11 03:11:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const input = require("readline-sync");
+//areaOfTriangle
+
+const base = input.question("What is the base of the triangle?\n")
+const height = input.question("What is the height of the triangle\n")
+console.log((base * height) / 2);
+// returns the min and max number of an array
+// function minMax(arr) {
+//   return [Math.min(...arr), Math.max(...arr)];
+// }
+// const num = input.question("Input numbers")
+// num = array
+// console.log(num.sort)
+
+// const yet = input.numbersse(fd)
